1f16a1b830742c2618539cafdcc2056859793a46
Fernanda Vilinga <fernandaVilinga0@gmail.com>
2026-06-28 01:20:32 +0100

    init: projeto React MukandaPrepa

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <h1 style={{ textAlign: "center", marginTop: "50px" }}>
+        Bem-vindo ao MukandaPrepa 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
